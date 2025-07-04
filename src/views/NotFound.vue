7b5f512ff2939df39254eef5f24a2624c4fe160c
GPHUC04 <127816230+GPHUC04@users.noreply.github.com>
2025-07-04 09:12:55 +0700

    commted

--- src/views/NotFound.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="page">
-    <p>
-      Oops, Không tìm thấy trang bạn yêu cầu. Vui lòng kiểm tra lại đường dẫn hoặc quay lại trang
-      chính.
-      <router-link to="/">Trang chủ</router-link>.
-    </p>
-  </div>
-</template>
